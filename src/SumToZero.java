3d9f110fe818131b73729c47ba8d5e3b147be780
Vincent Nguyen <vincentnguyen1732@gmail.com>
2014-10-17 15:00:10 -0700

    adding array problems

---
+++ src/SumToZero.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class SumToZero {
+
+    private SumToZero() {
+
+    }
+
+    public static void sumToZero(int[] inputArray) {
+        Arrays.sort(inputArray);
+        for (int i = 0; i < inputArray.length - 2; i++) {
+        	for (int j = i + 1; j < inputArray.length - 1; j++) {
+        		for (int k = j + 1; k < inputArray.length; k++) {
+        			if (inputArray[i] + inputArray[j] + inputArray[k] == 0) {
+        				System.out.println(inputArray[i] + " " + inputArray[j] + " " + inputArray[k]);
+        			}
+        		}
+        	}
+        }
+    }
+    
+    public static void main(String[] args) {
+        sumToZero(new int[]{2, 3, 1, -2, -1, 0, 2, -3, 0});
+    }
+}
